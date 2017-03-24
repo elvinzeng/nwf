65dc4e82381bc8d37105ec9ddaa672d1eb71073a
zenghui <zengh@idreamtech.com.cn>
2017-03-24 13:50:38 +0800

    add a view for helloworld module

--- nwf_modules/helloworld/HelloModController.lua
+++ nwf_modules/helloworld/HelloModController.lua
@@ -3,5 +3,5 @@ local helloModController = commonlib.gettable("nwf.controllers.HelloModControlle
 
 -- http://localhost:8099/helloMod/test1
 function helloModController.test1(ctx)
-    return "test", {message = "Hello, Elvin!"};
+    return "helloworld/hello", {message = "Hello, Elvin!"};
 end
--- nwf_modules/helloworld/install.sh
+++ nwf_modules/helloworld/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo helloworld module install...
+
+mkdir ../../view/helloworld
+cp hello.html ../../view/helloworld
