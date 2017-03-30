39736edd81ee6b299e9988feae044d5977846b68
zenghui <zengh@idreamtech.com.cn>
2017-03-30 17:00:37 +0800

    resolve a issue about dll can not be load

--- resources/scripts/start_win.bat
+++ resources/scripts/start_win.bat
@@ -1,6 +1,6 @@
 @echo off
 rem run in development mode
 rem npl -d bootstrapper="www/webapp.lua" port="8099" root="www/"  dev="%~dp0" servermode="true"
-SET PATH=%PATH%;./lib/dll/?.dll;
+SET cwd=%~dp0
+SET PATH=%PATH%;%cwd%lib\dll\;
 npls -d bootstrapper="www/webapp.lua" dev="%~dp0"
-
