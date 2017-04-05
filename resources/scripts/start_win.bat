a69ee9ea6c29277fc634229abf5c8e7bd2cba993
zenghui <zengh@idreamtech.com.cn>
2017-04-05 18:34:06 +0800

    update start_win.bat

--- resources/scripts/start_win.bat
+++ resources/scripts/start_win.bat
@@ -3,4 +3,5 @@ rem run in development mode
 rem npl -d bootstrapper="www/webapp.lua" port="8099" root="www/"  dev="%~dp0" servermode="true"
 SET cwd=%~dp0
 SET PATH=%PATH%;%cwd%lib\dll\;
-npls -d bootstrapper="www/webapp.lua" dev="%~dp0"
+rem npls -d bootstrapper="www/webapp.lua" dev="%~dp0"
+npl servermode="true" bootstrapper="www/webapp.lua" dev="%~dp0"
