a3ab49bada68fc45dbf85f1bba5c6876b93aff87
zenghui <zengh@idreamtech.com.cn>
2017-04-10 15:49:08 +0800

    skip server state check at start.sh exec if 'server.pid' not found

--- resources/scripts/start.sh
+++ resources/scripts/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd $(cd $(dirname $0) && pwd -P)
 
-flag=$(ps -ef | grep $(cat server.pid) | wc -l)
+if [ -e 'server.pid' ]; then
+    flag=$(ps -ef | grep $(cat server.pid) | wc -l)
+else
+    flag=0
+fi
+
 if [ $flag -gt 1 ]; then
 	echo server already running, operation cancelled.
 	exit;
