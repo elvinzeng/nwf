a183c37fd1f4c125a9d97d233ad69ccd2e2519f6
Elvin Zeng <zenghui@188.com>
2017-05-06 17:05:37 +0800

    switch submodule url to mirror on project update

--- resources/scripts/_update_project.sh
+++ resources/scripts/_update_project.sh
@@ -17,11 +17,15 @@ if [ $country_flag -ge 1 ]; then
     if [ -f "npl_packages/main/README.md" ]; then
         cd npl_packages/main
         git remote set-url origin https://git.oschina.net/elvinzeng/npl-main-packages.git
+        sed -i 's/https:\/\/github.com\/NPLPackages\/main/https:\/\/git.oschina.net\/elvinzeng\/npl-main-packages.git/' .gitmodules
+        sed -i 's/https:\/\/github.com\/NPLPackages\/main/https:\/\/git.oschina.net\/elvinzeng\/npl-main-packages.git/' .git/config
         cd $PROJECT_BASE_DIR
     fi
     if [ -f "npl_packages/nwf/README.md" ]; then
         cd npl_packages/nwf
         git remote set-url origin https://git.oschina.net/elvinzeng/nwf.git
+        sed -i 's/https:\/\/github.com\/elvinzeng\/nwf.git/https:\/\/git.oschina.net\/elvinzeng\/nwf.git/' .gitmodules
+        sed -i 's/https:\/\/github.com\/elvinzeng\/nwf.git/https:\/\/git.oschina.net\/elvinzeng\/nwf.git/' .git/config
         cd $PROJECT_BASE_DIR
     fi
 else
@@ -29,11 +33,15 @@ else
     if [ -f "npl_packages/main/README.md" ]; then
         cd npl_packages/main
         git remote set-url origin https://github.com/NPLPackages/main
+        sed -i 's/https:\/\/git.oschina.net\/elvinzeng\/npl-main-packages.git/https:\/\/github.com\/NPLPackages\/main/' .gitmodules
+        sed -i 's/https:\/\/git.oschina.net\/elvinzeng\/npl-main-packages.git/https:\/\/github.com\/NPLPackages\/main/' .git/config
         cd $PROJECT_BASE_DIR
     fi
     if [ -f "npl_packages/nwf/README.md" ]; then
         cd npl_packages/nwf
         git remote set-url origin https://github.com/elvinzeng/nwf.git
+        sed -i 's/https:\/\/git.oschina.net\/elvinzeng\/nwf.git/https:\/\/github.com\/elvinzeng\/nwf.git/' .gitmodules
+        sed -i 's/https:\/\/git.oschina.net\/elvinzeng\/nwf.git/https:\/\/github.com\/elvinzeng\/nwf.git/' .git/config
         cd $PROJECT_BASE_DIR
     fi
 fi
